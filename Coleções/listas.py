922be7db855d864af7555a18bdecdbb7fa97f4ba
Isabelly Florence <florenceisabelly@gmail.com>
2026-04-04 16:20:22 -0300

    primeiro commit

---
+++ Coleções/listas.py
@@ -0,0 +1,17 @@
+"""
+Listas
+
+Listas em Python funcionam como vetores/matrizes (arrays), com a diferença de serem
+DINÂMICO e também de podermos colocar QUALQUER tipo de dado.
+
+Linguagens C/Java
+    - Arrays possuem tamanho e tipo de dado fixo:
+    Ou seja, nestas linguagens se você criar um array do tipo int e com tamanho 5, esse array
+    será SEMPRE do tipo inteiro e poderá SEMPRE no máximo 5 valores.
+
+Python
+- Dinâmico: Não possui tamanho fixo, ou seja, podemos criar a lista e ir adicionando elementos
+- Qualquer tipo de dado: Não possuem tipo de dado fixo, ou seja, podemos colocar qualquer tipo de dado
+
+As listas são representadas por colchetes {}
+"""
---
+++ Variáveis, Estruturas Lógicas, Condicionais e de Repetição/tipo_boolean.py
@@ -0,0 +1,35 @@
+'''
+Tipo Boolean
+
+2 constantes. True ou False
+
+Sempre com a inicial Maiúscula
+'''
+
+ativo = True
+print(ativo)
+
+# NEGAÇÃO (not)
+# SE O VALOR FOR VERDADEIRO O RESULTADO SERÁ FALSE
+# SE O VALOR FOR FALSO O RESULTADO SERÁ TRUE
+
+ativo = False
+print(not ativo)
+
+# OU (or)
+# True or True = True
+# True or False = True
+# False or True = True
+# False or False = False
+
+logado = True
+print(ativo or logado)
+
+# E (and)
+# Ambos os valores devem ser verdadeiro
+# True and True = True
+# False and False = False
+# True and False = False
+# False and True= False
+
+print(ativo and logado)
